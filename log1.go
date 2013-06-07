53e938f16f500fee23a9bc39269d1c0deb2c2096
nsegreto <nsegreto@gmail.com>
2013-06-07 16:14:02 -0700

    TS060713B

--- log1.go
+++ log1.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"fmt"
 	"log"
-	"bufio"
+//	"bufio"
 )
 
 
@@ -35,20 +35,57 @@ To				string
 Payload			load
 }
 
+//Shaper is an interface and has a single function Area that returns an int.
+type Shaper interface {
+   Area() int
+   Shit() int
+}
+
+type Rectangle struct {
+   length, width int
+}
+
+//this function Area works on the type Rectangle and has the same function signature defined in the interface Shaper.  Therefore, Rectangle now implements the interface Shaper.
+func (r Rectangle) Area() int {
+   return r.length * r.width
+}
 
 
-
-
-
-
-
-
+func (r Rectangle) Shit() int {
+	return r.length + r.width
+	}
 
 func main() {
 
-	w:= bufio.NewWriter(os.Stdout)
-	w.WriteString("HELLO THERE \nOK\n\n");
+  r := Rectangle{length:5, width:3}
+   fmt.Println("Rectangle r details are: ", r)  
+   fmt.Println("Rectangle r's area is: ", r.Area())  
 
+   s := Shaper(r)
+   fmt.Println("Area of the Shape r is: ", s.Area())  
+   fmt.Println("Area of the Shape r is: ", s.Shit())  
+   
+   
+   
+   return;
+
+	cfgFile := "file.txt"
+	
+	f, err := os.OpenFile(cfgFile, os.O_CREATE| os.O_APPEND| os.O_RDWR, 0666) // For read access.
+	if err != nil {
+		   checkError(err)
+	}
+	
+  //	var l int
+	 myLog := log.New(f, "PREFIXIO ", log.Lshortfile |log.Ldate|log.Ltime  );
+ //	l, err = f.WriteString("HELLO THERE n");
+ //	fmt.Printf("length is :%d\n", l);
+ 	if( err != nil ){
+ 		checkError2(err);
+ 	}
+	myLog.Println("from logger");
+	//myFileAppendLine(cfgFile, "HELLO THERE \n");
+	
 	fmt.Printf("Flags:%q   Prefix:%q\n", log.Flags(), log.Prefix() );
 	os.Exit(0);
 	}
@@ -67,3 +104,20 @@ func checkError2(err error) {
 			}
 }
 
+func myFileAppendLine ( fName string, line string )  {
+	
+	f, err := os.OpenFile(fName, os.O_CREATE|os.O_APPEND|os.O_WRONLY , 0644)
+	checkError(err)
+	/*
+	if( err != nil ) {
+		f, err = os.Create(fName)
+		checkError(err)
+		}else {
+			_, err = f.Seek(0, os.SEEK_END)
+		}
+	*/
+	//_, err = f.Seek(0, os.SEEK_END)
+	_, err = f.WriteString(line)
+
+	f.Close()
+}
